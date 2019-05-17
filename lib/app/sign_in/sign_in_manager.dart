ac133d5ab55959bbf5af92e3999c805fc9e5d13a
bizz84 <bizz84@gmail.com>
2019-05-17 14:00:38 +0100

    Rename SignInBloc -> SignInManager

--- lib/app/sign_in/sign_in_manager.dart
+++ lib/app/sign_in/sign_in_manager.dart
@@ -5,8 +5,8 @@ import 'package:flutter/foundation.dart';
 import 'package:meta/meta.dart';
 import 'package:time_tracker_flutter_course/services/auth.dart';
 
-class SignInBloc {
-  SignInBloc({@required this.auth, @required this.isLoading});
+class SignInManager {
+  SignInManager({@required this.auth, @required this.isLoading});
   final AuthBase auth;
   final ValueNotifier<bool> isLoading;
 
--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:time_tracker_flutter_course/app/sign_in/email_sign_in_page.dart';
-import 'package:time_tracker_flutter_course/app/sign_in/sign_in_bloc.dart';
+import 'package:time_tracker_flutter_course/app/sign_in/sign_in_manager.dart';
 import 'package:time_tracker_flutter_course/app/sign_in/sign_in_button.dart';
 import 'package:time_tracker_flutter_course/app/sign_in/social_sign_in_button.dart';
 import 'package:time_tracker_flutter_course/common_widgets/platform_exception_alert_dialog.dart';
@@ -13,25 +13,26 @@ import 'package:time_tracker_flutter_course/services/auth.dart';
 class SignInPage extends StatelessWidget {
   const SignInPage({
     Key key,
-    @required this.bloc,
+    @required this.manager,
     @required this.isLoading,
   }) : super(key: key);
-  final SignInBloc bloc;
+  final SignInManager manager;
   final bool isLoading;
 
   static Widget create(BuildContext context) {
     final auth = Provider.of<AuthBase>(context);
     return Provider<ValueNotifier<bool>>(
       builder: (context) => ValueNotifier<bool>(false),
+      dispose: (context, valueNotifier) => valueNotifier.dispose(),
       child: Consumer<ValueNotifier<bool>>(
-        builder: (context, valueNotifier, _) => Provider<SignInBloc>(
+        builder: (context, valueNotifier, _) => Provider<SignInManager>(
               builder: (context) =>
-                  SignInBloc(auth: auth, isLoading: valueNotifier),
-              child: Consumer<SignInBloc>(
-                builder: (context, bloc, _) => ValueListenableBuilder<bool>(
+                  SignInManager(auth: auth, isLoading: valueNotifier),
+              child: Consumer<SignInManager>(
+                builder: (context, manager, _) => ValueListenableBuilder<bool>(
                       valueListenable: valueNotifier,
                       builder: (context, isLoading, _) => SignInPage(
-                            bloc: bloc,
+                            manager: manager,
                             isLoading: isLoading,
                           ),
                     ),
@@ -50,7 +51,7 @@ class SignInPage extends StatelessWidget {
 
   Future<void> _signInAnonymously(BuildContext context) async {
     try {
-      await bloc.signInAnonymously();
+      await manager.signInAnonymously();
     } on PlatformException catch (e) {
       _showSignInError(context, e);
     }
@@ -58,7 +59,7 @@ class SignInPage extends StatelessWidget {
 
   Future<void> _signInWithGoogle(BuildContext context) async {
     try {
-      await bloc.signInWithGoogle();
+      await manager.signInWithGoogle();
     } on PlatformException catch (e) {
       if (e.code != 'ERROR_ABORTED_BY_USER') {
         _showSignInError(context, e);
@@ -68,7 +69,7 @@ class SignInPage extends StatelessWidget {
 
   Future<void> _signInWithFacebook(BuildContext context) async {
     try {
-      await bloc.signInWithFacebook();
+      await manager.signInWithFacebook();
     } on PlatformException catch (e) {
       if (e.code != 'ERROR_ABORTED_BY_USER') {
         _showSignInError(context, e);
