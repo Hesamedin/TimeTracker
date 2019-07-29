6c2a35ba474888ebd2c3e9183e3b4b6ce19e4054
bizz84 <bizz84@gmail.com>
2019-07-29 16:04:23 +0100

    Adding the tabBuilder

--- lib/app/landing_page.dart
+++ lib/app/landing_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:time_tracker_flutter_course/app/home/home_page.dart';
 import 'package:time_tracker_flutter_course/app/home/jobs/jobs_page.dart';
 import 'package:time_tracker_flutter_course/app/sign_in/sign_in_page.dart';
 import 'package:time_tracker_flutter_course/services/auth.dart';
@@ -19,7 +20,7 @@ class LandingPage extends StatelessWidget {
             }
             return Provider<Database>(
               builder: (_) => FirestoreDatabase(uid: user.uid),
-              child: JobsPage(),
+              child: HomePage(),
             );
           } else {
             return Scaffold(
